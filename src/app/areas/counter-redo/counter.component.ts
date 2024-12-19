d562a37eaf9fb3f5f1a0e591f473258d294a339d
Joe Barkie <josephbarkie@hotmail.com>
2024-12-19 15:26:46 -0500

    Add links
    To UI and Prefs components.

--- src/app/areas/counter-redo/counter.component.ts
+++ src/app/areas/counter-redo/counter.component.ts
@@ -1,10 +1,18 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-counter',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <div>Counter stuff here</div> `,
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <div class="flex gap-6">
+      <a routerLink="ui">UI</a>
+      <a routerLink="prefs">Prefs</a>
+    </div>
+
+    <router-outlet />
+  `,
   styles: ``,
 })
 export class CounterComponent {}
