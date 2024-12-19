11e288e0ef13e7dd7f4cd93a6a5c19c3241133e7
Joe Barkie <josephbarkie@hotmail.com>
2024-12-19 15:26:33 -0500

    Add children routes
    For UI and Prefs components.

--- src/app/areas/counter-redo/counter.routes.ts
+++ src/app/areas/counter-redo/counter.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './counter.component';
+import { PrefsComponent } from './pages/prefs.component';
+import { UiComponent } from './pages/ui.component';
 export const COUNTER_ROUTES: Routes = [
   {
     path: '',
     component: CounterComponent,
-    children: [],
+    children: [
+      {
+        path: 'ui',
+        component: UiComponent,
+        children: [],
+      },
+      {
+        path: 'prefs',
+        component: PrefsComponent,
+        children: [],
+      },
+    ],
   },
 ];
